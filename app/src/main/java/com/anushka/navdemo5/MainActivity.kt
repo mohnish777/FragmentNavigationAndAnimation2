79abf5f2b5097e6264513792c94365ecfcf7c21e
mohnish777 <mohnishkumar_chittoory@condenast.com>
2025-01-16 22:05:01 +0530

    add lifecycle callbacks

--- app/src/main/java/com/anushka/navdemo5/MainActivity.kt
+++ app/src/main/java/com/anushka/navdemo5/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.anushka.navdemo5
 
+import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Log.d("LifeCycle", "onCreate: onCreate")
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart: onStart")
+        super.onStart()
+    }
+
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume: onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause: onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop: onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle", "onRestart: onRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy: onDestroy")
+        super.onDestroy()
     }
 }
